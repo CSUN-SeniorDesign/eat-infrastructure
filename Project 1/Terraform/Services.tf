403de6275597792923eb1eed6bb925e3caaaa1e1
Tyler Kluszczynski <tylerkluszczynski@gmail.com>
2018-09-11 20:46:04 -0700

    Setting up basic Services
    Just setting up basic load balancer service. It needs an internet gateway to function properly, so it hasn't been tested yet.

---
+++ Project 1/Terraform/Services.tf
@@ -0,0 +1,11 @@
+resource "aws_lb" "Load-Balancer"{
+  name               = "Load-Balancer"
+  internal           = false
+  load_balancer_type = "application"
+
+  enable_deletion_protection = true
+  
+  subnets            = ["${aws_subnet.pubsubnet1.id}","${aws_subnet.pubsubnet2.id}","${aws_subnet.pubsubnet3.id}"]
+
+}
+
