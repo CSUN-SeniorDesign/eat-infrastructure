471c8d8390449cfc4150d25b846b73265a832d87
Tyler Kluszczynski <tylerkluszczynski@gmail.com>
2018-09-13 15:42:19 -0700

    Readded subnetting after accidental deletion. Testing Route 53
    Subnetting also set up correctly. Added a route table that is now set up correctly.
    Set up some basic Route 53.

--- Project 1/Terraform/Services.tf
+++ Project 1/Terraform/Services.tf
@@ -6,6 +6,20 @@ resource "aws_lb" "Load-Balancer"{
   enable_deletion_protection = false
   
   subnets            = ["${aws_subnet.pubsubnet1.id}","${aws_subnet.pubsubnet2.id}","${aws_subnet.pubsubnet3.id}"]
-
 }
 
+resource "aws_route53_zone" "main" {
+  name         = "fa480.club"
+}
+
+resource "aws_route53_zone" "blog" {
+  name         = "blog.fa480.club"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "www.fa480.club"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.0.0.1"]
+}
