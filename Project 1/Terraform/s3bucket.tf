8477a75127931cee4c80eba6691397581479e4f0
BriantheAvenger <briansandoval2595@gmail.com>
2018-09-17 14:18:46 -0700

    s3 files

---
+++ Project 1/Terraform/s3bucket.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "csuneat"
+  acl = "private"
+
+  tags {
+    Name = "Team Eat Bucket"
+    Environment = "Dev"
+  }
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state"{
+  name = "terraform-state-lock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags {
+    Name = "DynamoDB Terraform Lock Table"
+  }
+}
