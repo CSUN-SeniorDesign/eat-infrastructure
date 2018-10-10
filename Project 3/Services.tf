fc5e7e321fc95a27ceadd4356667f84b8151b46c
Tyler Kluszczynski <tylerkluszczynski@gmail.com>
2018-10-09 18:08:46 -0700

    Adding more stuff for lambda compatability.

--- Project 3/Services.tf
+++ Project 3/Services.tf
@@ -424,4 +424,23 @@ resource "aws_lambda_function" "test-lambda" {
 }
 
 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.test-lambda.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".txt"
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.test-lambda.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.bucket.arn}"
+}
+
+
 
