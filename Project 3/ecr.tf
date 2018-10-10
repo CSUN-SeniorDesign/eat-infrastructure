783bc1766b6f23df66c04ecda7dc483c378a9eb6
ErikBlomquist <koerk6@gmail.com>
2018-10-10 09:42:01 -0700

    ECR added + ECS adjusted

---
+++ Project 3/ecr.tf
@@ -0,0 +1,10 @@
+# Create ECR
+resource "aws_ecr_repository" "beats_repo" {
+  name = "beats_repo"
+}
+
+# Output URL
+output "beats-repository-URL" {
+  value = "${aws_ecr_repository.beats_repo.repository_url}"
+
+}
