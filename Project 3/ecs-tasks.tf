818a871f0313c425133d4e4cab1576c1e317f2bb
ErikBlomquist <koerk6@gmail.com>
2018-10-15 14:03:11 -0700

    ECS updated

--- Project 3/ecs-tasks.tf
+++ Project 3/ecs-tasks.tf
@@ -1,14 +1,25 @@
 resource "aws_ecs_task_definition" "service" {
   family                = "service"
   container_definitions = "${file("task-definitions/service.json")}"
-
   volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
+    name      = "service"
+    host_path = "/ecs/beats_service"
   }
-
   placement_constraints {
     type       = "memberOf"
     expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
   }
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "service"
+  cluster         = "${aws_ecs_cluster.beats-cluster.id}"
+  task_definition = "${aws_ecs_task_definition.service.arn}"
+
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+    }
+
 }
