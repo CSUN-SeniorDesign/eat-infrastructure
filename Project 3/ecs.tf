934c199de0de3e821890ff4922008b6aac4f84b8
ErikBlomquist <koerk6@gmail.com>
2018-10-19 21:43:31 -0700

    Final adjustments

--- Project 3/ecs.tf
+++ Project 3/ecs.tf
@@ -1,26 +1,25 @@
-
 # ECS launch configuration
 resource "aws_launch_configuration" "ecs-launch-configuration" {
-name                  = "ecs-launch-configuration"
-image_id              = "ami-00430184c7bb49914"
-security_groups       = ["${aws_security_group.NATSG.id}"]
-instance_type         = "t2.micro"
-key_name              = "${aws_key_pair.deployer.key_name}"
-iam_instance_profile  = "${aws_iam_instance_profile.ecs-instance-profile.id}"
-user_data             = "#!/bin/bash\necho 'ECS_CLUSTER=beats-cluster' > /etc/ecs/ecs.config\nstart ecs"
-
+  name_prefix           = "ecs-cluster-launch"
+  image_id              = "ami-00430184c7bb49914"
+  security_groups       = ["${aws_security_group.NATSG.id}"]
+  instance_type         = "t2.micro"
+  key_name              = "${aws_key_pair.deployer.key_name}"
+  iam_instance_profile  = "${aws_iam_instance_profile.ecs-instance-profile.id}"
+  user_data             = "#!/bin/bash\necho 'ECS_CLUSTER=beats-cluster' > /etc/ecs/ecs.config\nstart ecs"
 root_block_device {
-volume_type = "standard"
-volume_size = 8
-delete_on_termination = true
+  volume_type = "standard"
+  volume_size = 8
+  delete_on_termination = true
 }
 
 lifecycle {
-create_before_destroy = true
+  create_before_destroy = true
 }
-associate_public_ip_address = "false"
+  associate_public_ip_address = "false"
 }
 
+# ECS ASG
 resource "aws_autoscaling_group" "ecs-autoscaling-group" {
   name = "ecs-autoscaling-group"
   max_size = "2"
@@ -29,10 +28,7 @@ resource "aws_autoscaling_group" "ecs-autoscaling-group" {
   vpc_zone_identifier = ["${aws_subnet.privsubnet1.id}"]
   launch_configuration = "${aws_launch_configuration.ecs-launch-configuration.name}"
   health_check_type = "ELB"
-
-  target_group_arns = ["${aws_alb_target_group.HTTP-Group.arn}"]
-
-
+  target_group_arns = ["${aws_alb_target_group.production-HTTP-Group.arn}"]
 tag {
   key = "Name"
   value = "-beats-ecs-asg"
@@ -40,6 +36,7 @@ tag {
 }
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "beats-cluster" {
   name = "beats-cluster"
 }
